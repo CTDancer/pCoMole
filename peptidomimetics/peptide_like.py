96c7a83da176f09953781e75dfd14bfa959f2bd2
TongChen <89793506+CTDancer@users.noreply.github.com>
2026-01-27 11:55:57 -0500

    Delete peptidomimetics/peptide_like.py

--- peptidomimetics/peptide_like.py
+++
@@ -1,7 +0,0 @@
-from cope_models.objectives import analyze_peptide_likeness
-
-seq = '''
-CNC(=O)[C@H1](CC1=CC=CC=C1)N(C)C(C)O[C@H1](CCCC=O)NCN(C)C(C)[C@H1](CC2=CC=CC=C2)N(C)C(=O)[C@H1]=O
-'''
-res = analyze_peptide_likeness(seq)
-print(res)
